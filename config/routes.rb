a38281edb6e51ff4e435ede674f2af4b7f41f8c0
Liseth Briceño <lgbricenoa@unal.edu.co>
2017-09-24 21:59:46 -0500

    cambiando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-    get 'test' => 'users#testSession'
+    get 'authorize' => 'users#testSession'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	post 'authenticate', to: 'authentication#authenticate'
