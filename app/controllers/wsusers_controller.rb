ebf965b57246ce7b54de119fc4f9dceb4bfa7ee7
Miguel Ballén <migaballengal@unal.edu.co>
2017-11-01 11:37:08 -0500

    Feat: Controller method and route defined

--- app/controllers/wsusers_controller.rb
+++ app/controllers/wsusers_controller.rb
@@ -1,2 +1,15 @@
 class WsusersController < ApplicationController
+  soap_service namespace: 'urn:WashOutUser', camelize_wsdl: :lower
+
+  soap_action "check",
+              :args => {:email => :string},
+              return => :boolean
+
+  def check
+    @req_mail = params[:email]
+    exists  = false
+    if (Users.exists?(email: @req_mail))
+      exists = true
+    end
+    render :soap => exists
 end
