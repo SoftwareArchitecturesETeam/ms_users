561799051a71183cd4b43f44be82610cf011f967
David Beltrán <debeltranc@unal.edu.co>
2017-11-15 17:17:39 -0500

    fix: fix in rancher compose

---
+++ start2.sh
@@ -0,0 +1,1 @@
+./rancher-compose --project-name sa-ms-users --url http://192.168.99.100:8080/v1/projects/1a5 --access-key B803C126542810692F29 --secret-key jDKFnV7RWtLD8GSPthEogNWzDyQb7b7NiP9pFve1 -f docker-compose.yml --verbose up -d --force-upgrade --confirm-upgrade
